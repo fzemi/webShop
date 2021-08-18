6e49fdeecda7e81171052a0ae0cf7fa81906685a
fzemi <filipzem11@gmail.com>
2021-08-18 14:40:53 +0200

    Added contact page

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,17 +1,17 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#"><img src="../assets/shoping_cart.png"/></a>
+      <router-link class="navbar-brand" to="/"><img src="../assets/shoping_cart.png"/></router-link>
       <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
+            <router-link class="nav-link" :class="{active: $route.name === 'Shop'}" to="/">Home</router-link>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">About</a>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" to="/contact">Contact</router-link>
+            <router-link class="nav-link" :class="{active: $route.name === 'Contact'}" to="/contact">Contact</router-link>
           </li>
         </ul>
       </div>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,32 @@
 <template>
-  <div class="contact">
-    <h1>This is an contact page</h1>
+  <div class="container">
+    <img src="../assets/at_symbol.png" style="padding-top: 20px;"/>
+    <h1 style="padding-top: 25px; padding-bottom: 10px;">CONTACT</h1>
+    <p>divante@divante.pl</p><br/>
+    <p>tel 111 111 111</p>
+    <p>tel 111 111 111</p><br/>
+    <p>Dmowskiego</p>
+    <p>Wrocław</p>
   </div>
 </template>
+
+
+<style scoped>
+
+.container {
+  background-color: aliceblue;
+  display: block;
+  text-align: center;
+  width: 500px;
+  height: 400px;
+  margin-top: 100px;
+  border-radius: 15px;
+}
+
+p {
+  margin-top: 0px;
+  margin-bottom: 0px;
+  color: rgba(162, 162, 162, 1);
+}
+
+</style>
